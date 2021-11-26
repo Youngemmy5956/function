1ee1ec287809d8afc1d0f80d7153482709266b5a
Nwamini Emmanuel Onyedikachi <85987314+Youngemmy5956@users.noreply.github.com>
2021-11-26 12:06:30 +0100

    Add files via upload

---
+++ function/js/app.js
@@ -0,0 +1,130 @@
+
+
+    let users = [
+        {
+          username: 'larry',
+          email: 'larry@foo.com',
+          yearsExperience: 22.1,
+          favoriteLanguages: ['Perl', 'Java', 'C++'],
+          favoriteEditor: 'Vim',
+          hobbies: ['Fishing', 'Sailing', 'Hiking'],
+          hometown: {
+            city: 'San Francisco',
+            state: 'CA'
+          }
+        },
+        {
+          username: 'jane',
+          email: 'jane@test.com',
+          yearsExperience: 33.9,
+          favoriteLanguages: ['Haskell', 'Clojure', 'PHP'],
+          favoriteEditor: 'Emacs',
+          hobbies: ['Swimming', 'Biking', 'Hiking'],
+          hometown: {
+            city: 'New York',
+            state: 'NY'
+          }
+        },
+        {
+          username: 'sam',
+          email: 'sam@test.com',
+          yearsExperience: 8.2,
+          favoriteLanguages: ['JavaScript', 'Ruby', 'Python', 'Go'],
+          favoriteEditor: 'Atom',
+          hobbies: ['Golf', 'Cooking', 'Archery'],
+          hometown: {
+            city: 'Fargo',
+            state: 'SD'
+          }
+        },
+        {
+          username: 'anne',
+          email: 'anne@test.com',
+          yearsExperience: 4,
+          favoriteLanguages: ['C#', 'C++', 'F#'],
+          favoriteEditor: 'Visual Studio Code',
+          hobbies: ['Tennis', 'Biking', 'Archery'],
+          hometown: {
+            city: 'Albany',
+            state: 'NY'
+          }
+        },
+        {
+          username: 'david',
+          email: 'david@test.com',
+          yearsExperience: 12.5,
+          favoriteLanguages: ['JavaScript', 'C#', 'Swift'],
+          favoriteEditor: 'VS Code',
+          hobbies: ['Volunteering', 'Biking', 'Coding'],
+          hometown: {
+            city: 'Los Angeles',
+            state: 'CA'
+          }
+        }
+      ];
+
+
+
+    //   function printEmails(users){
+    //       for(let i = 0; i < users.length; i++){
+    //           console.log(users[i].email);
+
+    //       }
+    //   }
+
+
+    //   function printHobbies(){
+    //        for(let i =0; i < users.length; i++){
+    //            for(let j = 0; j < users[i].hobbies.length; j++){
+    //                console.log(users[i].hobbies[j]);
+    //            }
+    //        }
+    //   }
+
+
+        //  function findHometownByState(state){
+        //    for(let i = 0; i < users.length; i++){
+        //      if(users[i].hometown.state === state){
+        //        return users[i].hometown.city 
+        //      }
+        //    }
+        //  }
+
+        // function allLanguages(){
+        //     let languages = [];
+        //     for(let i = 0; i < users.length; i++){
+        //         for(let j = 0; j < users[i].favoriteLanguages.length; j++){
+        //             languages.push(users[i].favoriteLanguages[j]);
+        //         }
+        //         return languages;
+        //     }
+        // }
+
+
+        // function hasFavoriteEditor(){
+        //     let favoriteEditor = [];
+        //     let editors = [];
+
+        //     for(let i = 0; i < users.length; i++){
+        //         editors.push(users[i].favoriteEditor);
+        //         if(favoriteEditor === editors){
+        //            return true 
+        //         }
+        //          else if(favoriteEditor !== editors){
+        //             return false;
+        //         }
+        //         }
+        //     }
+
+
+        // function findByUsername(){
+        //     let username = prompt('Enter a username');
+        //     for(let i = 0; i < users.length; i++){
+        //         if(users[i].username === username){
+        //             return users[i];
+        //         }
+        //     }
+
+            
+        // }
+        
